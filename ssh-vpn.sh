29e8ba2eaad71697331180b26df3c85d28ee8699
Mr.Trick <laptopku404@gmail.com>
2022-04-13 18:20:04 +0800

    sc

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -1,8 +1,33 @@
 #!/bin/bash
-# By Horasss
-#
-# ==================================================
+if [ "${EUID}" -ne 0 ]; then
+  echo "You need to run this script as root"
+  exit 1
+fi
+if [ "$(systemd-detect-virt)" == "openvz" ]; then
+  echo "OpenVZ is not supported"
+  exit 1
+fi
+apt -y install python
+apt -y install tmux
+apt -y install ruby
+gem install lolcat
+sudo snap install lolcat -y
+sudo apt-get install figlet -y
 
+# install
+apt install neofetch
+apt-get --reinstall --fix-missing install -y linux-headers-cloud-amd64 bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
+echo "clear" >> .profile
+echo "neofetch" >> .profile
+
+cd /usr/bin
+wget -O menu "https://raw.githubusercontent.com/rajakapur/onesc/main/menu.sh"
+wget -O banner "https://raw.githubusercontent.com/rajakapur/onesc/main/banner.sh"
+chmod +x menu
+chmod +x banner
+mkdir /var/lib/banner-name;
+echo -e "Premium" >> /var/lib/banner-name/banner
+clear
 # initializing var
 export DEBIAN_FRONTEND=noninteractive
 MYIP=$(wget -qO- ipinfo.io/ip);
@@ -82,9 +107,9 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git lsof
 echo "clear" >> .profile
 echo "neofetch --ascii_distro SliTaz" >> .profile
-echo "echo -e '\e[35m  Script Premium VPNshopee By \e[32mRARE \e[0m'" >> .profile
+echo "echo -e '\e[35m  Script Premium MR.TRICK By \e[32mMR \e[0m'" >> .profile
 echo "echo -e '\e[35m Sila Type: info, untuk melihat info \e[0m'" >> .profile
-echo "echo -e '\e[35m    Telegram:\e[0m \e[32m@vpnshopee \e[0m'" >> .profile
+echo "echo -e '\e[35m    Telegram:\e[0m \e[32m@MrTricksiak \e[0m'" >> .profile
 echo "echo ''" >> .profile
 echo "echo -e '\e[35m  Type\e[5m \e[32mmenu\e[0m \e[35mUntuk Melihat Menu VPS anda \e[0m'" >> .profile
 echo "echo ''" >> .profile
--- update.sh
+++ update.sh
@@ -15,5 +15,56 @@ echo -e "$green   =============================================$NC"
 echo -e "$green                    $Blink UPDATE SCRIPT$NC               $NC"
 echo -e "$green   =============================================$NC"
 sleep 5
-wget https://raw.githubusercontent.com/rajakapur/onesc/main/update.sh && chmod +x update.sh && screen -S update ./update.sh
-rm -f /root/update.sh
+rm -rf menu
+rm -rf m-sshovpn
+rm -rf m-wg
+rm -rf m-ssr
+rm -rf v2ray-vmess
+rm -rf v2ray-vless
+rm -rf xray-vmess
+rm -rf xray-vless
+rm -rf xray-xtls
+rm -rf m-trojan
+rm -rf m-system
+
+# download menu
+cd /usr/bin
+wget -O menu "https://raw.githubusercontent.com/nikstore/scnew/main/update/menu.sh"
+wget -O m-sshovpn "https://raw.githubusercontent.com/nikstore/scnew/main/update/m-sshovpn.sh"
+wget -O m-wg "https://raw.githubusercontent.com/nikstore/scnew/main/update/m-wg.sh"
+wget -O m-ssr "https://raw.githubusercontent.com/nikstore/scnew/main/update/m-ssr.sh"
+wget -O v2ray-vmess "https://raw.githubusercontent.com/nikstore/scnew/main/update/v2ray-vmess.sh"
+wget -O v2ray-vless "https://raw.githubusercontent.com/nikstore/scnew/main/update/v2ray-vless.sh"
+wget -O xray-vmess "https://raw.githubusercontent.com/nikstore/scnew/main/update/xray-vmess.sh"
+wget -O xray-vless "https://raw.githubusercontent.com/nikstore/scnew/main/update/xray-vless.sh"
+wget -O xray-xtls "https://raw.githubusercontent.com/nikstore/scnew/main/update/xray-xtls.sh"
+wget -O m-trojan "https://raw.githubusercontent.com/nikstore/scnew/main/update/m-trojan.sh"
+wget -O m-system "https://raw.githubusercontent.com/nikstore/scnew/main/update/m-system.sh"
+
+
+chmod +x menu
+chmod +x m-sshovpn
+chmod +x m-wg
+chmod +x m-ssr
+chmod +x v2ray-vmess
+chmod +x v2ray-vless
+chmod +x xray-vmess
+chmod +x xray-vless
+chmod +x xray-xtls
+chmod +x m-trojan
+chmod +x m-system
+sleep 2
+clear
+echo -e "$green   =============================================$NC"
+echo -e "$green                    $Blink SETTLE UPDATE$NC               $NC"
+echo -e "$green   =============================================$NC"
+sleep 5
+sl-download-info
+clear
+echo " Fix minor Bugs"
+echo " Now You Can Change Port Of Some Services"
+echo " Reboot 5 Sec"
+sleep 5
+rm -f update.sh
+reboot
+
