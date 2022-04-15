548dea132dfc2457a0aab735b7cd686f79a84cbc
Mr.Trick <laptopku404@gmail.com>
2022-04-15 15:18:54 +0800

    sc

--- grpc/addgrpc.sh
+++ grpc/addgrpc.sh
@@ -24,6 +24,8 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
 		fi
 	done
 uuid=$(cat /proc/sys/kernel/random/uuid)
+read -p "Expired (Days) : " masaaktif
+hariini=`date -d "0 days" +"%Y-%m-%d"`
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
 sed -i '/#vmessgrpc$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","alterId": '"0"',"email": "'""$user""'"' /etc/xray/vmessgrpc.json
@@ -74,6 +76,7 @@ echo -e "================================="
 echo -e "Link VLess GRPC  : "
 echo -e "${vlesslink1}"
 echo -e "================================="
+echo -e "Created        : $hariini"
 echo -e "Expired On     : $exp"
 echo -e "=================================" 
 echo -e " Script Mod By SL"
