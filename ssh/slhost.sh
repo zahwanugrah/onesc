2f1e789b540cc03ff19ebcd6682bf36fe9d5a0f3
zahwanugrah <39833642+zahwanugrah@users.noreply.github.com>
2022-07-29 16:18:09 +0700

    Update slhost.sh

--- ssh/slhost.sh
+++ ssh/slhost.sh
@@ -20,11 +20,11 @@ echo "$SUB_DOMAIN" >> /etc/v2ray/domain
 #
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c5)
 subsl=$(</dev/urandom tr -dc a-x0-9 | head -c5)
-DOMAIN=autosc.me
-SUB_DOMAIN=${sub}.autosc.me
-NS_DOMAIN=${subsl}.autosc.me
-CF_ID=awaledyan@gmail.com
-CF_KEY=7c9a592ee3186932780cffb3f1b6dbfee8084
+DOMAIN=mantapxsl.my.id
+SUB_DOMAIN=onichan-${sub}.mantapxsl.my.id
+NS_DOMAIN=dns-${sub}.mantapxsl.my.id
+CF_ID=slinfinity69@gmail.com
+CF_KEY=dd2c5e0313f122b3c1833471d469b1025f492
 #DOMAIN=akbar-store.me
 #SUB_DOMAIN=${sub}.akbar-store.me
 #NS_DOMAIN=${sub}.akbar-store.me
